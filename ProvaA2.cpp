320aba16637ae2fc67fc94ee7ffbc98bfc08f839
carlos-28 <abel.road@gmail.com>
2015-06-13 14:48:22 -0300

    Adicionado iluminação com glMaterialfv e correção de bug na calcada
    Não consegui fazer diminuir e aumentar a iluminação do sol
    Falta: -> String na caçamba do caminhão
    -> Modo 'primeira pessoa'
    -> Aumentar e diminuir iluminação do sol
    -> Texturas

--- ProvaA2.cpp
+++ ProvaA2.cpp
@@ -50,11 +50,14 @@ float anguloCubo = 0;
 float incrAnguloCubo = 1;
 
 // Mensagem
-char *str = "camera (%.2f, %.2f, %.2f)  Angulo: %.2f  Velocidade: %.2f  Farol: Poste: ";
+char *str = "Camera (%.2f, %.2f, %.2f)  Angulo: %.2f  Velocidade: %d  Farol: %c Poste: %c";
 
 // Escala da fonte
 float tamFonte = 0.1;
 float incrFonte = 0;
+
+//Velocidade de movimentacao
+int velocidade = 1;
 
 // Largura e altura da viewport
 int largura_vp, altura_vp;
@@ -67,22 +70,73 @@ int i;
 
 // Variaveis de luz
 bool farolLuz = true;
+bool posteLuz = true;
 
-float posicaoLuz1[]  = {-1.5, 5, -7.5, 1.0};
-float posicaoLuz2[] = {1.5, 5, -7.5, 1.0};
+char posteChar = 'L';
+char farolChar = 'L';
+
+float posicaoLuz1[]  = {-1.5, 5, -7.5, 1};
+float posicaoLuz2[] = {1.5, 5, -7.5, 1};
+float posicaoLuz3[] = {3, 20, 0, 1};
+float posicaoLuz4[] = {3, 20, -100, 1};
+float posicaoLuz5[] = {3, 20, -200, 1};
+float posicaoLuz6[] = {3, 20, -300, 1};
+float posicaoLuz7[] = {3, 20, -400, 1};
+float posicaoLuz8[] = {3, 20, -500, 1};
 float direcaoSpot0[] = { 0, 0, -1};
 
 float corEstrada[] = {0.5, 0.5, 0.5, 1.0};
-float corCalcada[] = {1.0, 0.3, 0.3, 1.0};
-float corGrama[] = {0.0, 0.8, 0.0, 1.0};
+float corCalcada[] = {0.3, 0.3, 0.3, 1.0};
+float corGrama[] = {117/255.0, 155/255.0, 63/255.0, 1.0};
+float corCeu[] = {123.0/255, 217.0/255, 234.0, 1.0};
+float corCopa[] = {0, 1, 0, 1.0};
+float corCaule[] = {128.0/255, 64.0/255, 0, 1.0};
+float corPreto[] = {0, 0, 0, 1.0};
+float corVermelho[] = {1, 0, 0, 1.0};
+float corChassi[] = {157.0/255, 150.0/255, 52.0/255, 1.0};
+float corFarolOn[] = {1, 1, 0, 1.0};
+float corFarolOff[] = {0.5, 0.5, 0.5, 1.0};
+float corPoste[] = {0.8, 0.8, 0.8, 1.0};
+float corLuminaria[] = {0.8, 0.8, 0, 1.0};
+float corCacamba[] = {255/255.0, 128/255.0, 0, 1.0};
 
 float luzAmbiente[] = {0.3, 0.3, 0.3, 1.0}; 
-float consSol = 1.0;
+float conSol = 1.0;
 float luzSolAmbiente[] = {0.3, 0.3, 0.3, 1.0};
-float luzSolDifusa[] = {consSol, consSol, consSol, 1.0};
+float luzSolDifusa[] = {conSol, conSol, conSol, 1.0};
 float posicaoLuz0[] = {150.0, 150.0, -150.0, 0.0};
 float desligado[]    = { 0, 0, 0, 0 };
 float branco[]    = { 1, 1, 1, 1 };
+
+
+void calculaNormal(float vertice1[], // (x1, y1, z1) do vertice 1
+                   float vertice2[], // (x2, y2, z2) do vertice 2
+                   float vertice3[], // (x3, y3, z3) do vertice 3
+                   float normal[])   // (xn, yn, zn) da normal
+{
+  float Qx = vertice2[0] - vertice1[0];  // x2 – x1
+  float Qy = vertice2[1] - vertice1[1];  // y2 – y1
+  float Qz = vertice2[2] - vertice1[2];  // z2 – z1
+  float Px = vertice3[0] - vertice2[0];  // x3 – x2
+  float Py = vertice3[1] - vertice2[1];  // y3 – y2
+  float Pz = vertice3[2] - vertice2[2];  // z3 – z2
+
+  normal[0] = Qy*Pz - Qz*Py;
+  normal[1] = Qz*Px - Qx*Pz; 
+  normal[2] = Qx*Py - Qy*Px; 
+}
+
+void normaliza(float vetor[],       // (xn, yn, zn) não normalizado
+               float normalizado[]) // (xn, yn, zn) normalizado
+{
+  float c = sqrt(vetor[0]*vetor[0] +  // xn * xn +
+                 vetor[1]*vetor[1] +  // yn * yn +
+                 vetor[2]*vetor[2]);  // zn * zn
+
+  normalizado[0] = vetor[0] / c;
+  normalizado[1] = vetor[1] / c;
+  normalizado[2] = vetor[2] / c;
+}
 
 void restauraProjecaoPerspectiva()
 {
@@ -126,62 +180,78 @@ void iniciaProjecaoOrtografica2D() {
 	glLoadIdentity();
 }
 
-void estrada(void){
-	
-	// Desenha a rua
-   glBegin(GL_POLYGON);
-   //glColor3f(0.5f, 0.5f, 0.5f);
-   glVertex3i(-10, 0, -600);
-   glVertex3i(-10, 0,  600);
-   glVertex3i( 10, 0,  600);
-   glVertex3i( 10, 0, -600);
-   glEnd();
+void estrada(void){	
+	//glColor3f(0.5f, 0.5f, 0.5f);
+	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, corEstrada);
+	for (i=-600; i<=600; i+=10){
+		glBegin(GL_POLYGON);
+		glNormal3f(0, 1, 0);
+		glVertex3i(-10, 0, i);
+		glVertex3i(-10, 0, i+10);
+		glVertex3i(10, 0, i+10);
+		glVertex3i(10, 0, i);
+		glEnd();
+	}
 }
 
 void calcada(void){
-	
-	 // Desenha a calcada esquerdo
-   glBegin(GL_POLYGON);
-   //glColor3f(0.3f, 0.3f, 0.3f);
-   glVertex3i(-10, 0, -600);
-   glVertex3i(-15, 0, -600);
-   glVertex3i(-15, 0,  600);
-   glVertex3i(-10, 0,  600);
-   glEnd();
+	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, corCalcada);
+	for (i=-600; i<=600; i+= 10){
+		// Desenha a calcada esquerdo
+   		glBegin(GL_POLYGON);
+   		glNormal3f(0, 1, 0);
+   		glVertex3i(-15, 0, i);
+   		glVertex3i(-15, 0, i+10);
+   		glVertex3i(-10, 0,  i+10);
+   		glVertex3i(-10, 0,  i);
+   		glEnd();
    
-   // Desenha a calcada direita
-   glBegin(GL_POLYGON);
-   //glColor3f(0.3f, 0.3f, 0.3f);
-   glVertex3i(10, 0, -600);
-   glVertex3i(15, 0, -600);
-   glVertex3i(15, 0,  600);
-   glVertex3i(10, 0,  600);
-   glEnd();
+    	// Desenha a calcada direita
+   		glBegin(GL_POLYGON);
+   		glNormal3f(0, 1, 0);
+   		glVertex3i(10, 0, i);
+   		glVertex3i(10, 0, i+10);
+   		glVertex3i(15, 0,  i+10);
+   		glVertex3i(15, 0,  i);
+   		glEnd();
+	}
 }
 
 void grama(void){
-   // Gramado
-   glBegin(GL_QUADS);
-   //glColor3f(117/255.0, 155/255.0, 63/255.0);
-   glVertex3i(-600, -1, -600);
-   glVertex3i( 600, -1, -600);
-   glVertex3i( 600, -1, 600);
-   glVertex3i(-600, -1, 600);
-   glEnd();
+	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, corGrama);
+ 	//Desenha grama esquerda
+	glBegin(GL_POLYGON);
+	glNormal3f(0, 1, 0);
+	glVertex3i(-215, 0, -600);
+	glVertex3i(-215, 0, 600);
+	glVertex3i(-15, 0, 600);
+	glVertex3i(-15, 0, -600);
+	glEnd();
+	
+	//Desenha grama direita
+	glBegin(GL_POLYGON);
+	glNormal3f(0, 1, 0);
+	glVertex3i(15, 0, -600);
+	glVertex3i(15, 0, 600);
+	glVertex3i(215, 0, 600);
+	glVertex3i(215, 0, -600);
+	glEnd();
 }
 
 void ceu(void){
+	glMaterialfv(GL_FRONT_AND_BACK , GL_AMBIENT_AND_DIFFUSE, corCeu);
 	glPushMatrix();
-	glColor3f(0.3f, 0.3f, 0.8f);
+	//glColor3f(0.3f, 0.3f, 0.8f);
 	glTranslatef(0, -300, 0);
 	glutSolidSphere(650, 256, 256);
 	glPopMatrix();
 }
 
 void chassi(void){
+	glMaterialfv(GL_FRONT_AND_BACK , GL_AMBIENT_AND_DIFFUSE, corChassi);
 	//Desenha chassi caminhao
 	glBegin(GL_POLYGON);
-	glColor3f(0.8f, 0.8f, 0.0f);
+	glNormal3f(0, 1, 0);
 	glVertex3i(6, 2, 0);
 	glVertex3i(-6, 2, 0);
 	glVertex3i(-6, 2, 20);
@@ -194,7 +264,7 @@ void chassi(void){
 }
 
 void eixo(void){
-	glColor3f(1.0f, 0.0f, 0.0f);
+	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, corVermelho);
 	glPushMatrix();
 	glRotatef(90, 0, 1, 0);
 	drawCylinder(1,16,11,0.4);
@@ -202,7 +272,7 @@ void eixo(void){
 }
 
 void roda(void){
-	glColor3i(1.0f, 1.0f, 1.0f);
+	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, corPreto);
 	glPushMatrix();
 	glRotatef(90, 0, 1, 0);
 	glutWireTorus(1, 2, 20, 16);
@@ -210,9 +280,10 @@ void roda(void){
 }
 
 void cabineEsq1(void){
+	glMaterialfv(GL_FRONT_AND_BACK , GL_AMBIENT_AND_DIFFUSE, corChassi);
 	//Desenha Cabine lado esquerdo Parte 1
 	glBegin(GL_POLYGON);
-	glColor3f(0.8f, 0.5f, 0.0f);
+	glNormal3f(-1, 0, 0);
 	glVertex3i(-1, 8, -5);
 	glVertex3i(-1, 3, -5);
 	glVertex3i(-1, 3, 4);
@@ -221,6 +292,7 @@ void cabineEsq1(void){
 	
 	//Desenha Cabine lado esquerdo Parte 2
 	glBegin(GL_POLYGON);
+	glNormal3f(-1, 0, 0);
 	glVertex3i(-1, 12, 3);
 	glVertex3i(-1, 8, 3);
 	glVertex3i(-1, 8, 4);
@@ -229,6 +301,7 @@ void cabineEsq1(void){
 		
 	//Desenha Cabine lado esquerdo Parte 3
 	glBegin(GL_POLYGON);
+	glNormal3f(-1, 0, 0);
 	glVertex3i(-1, 13, -1);
 	glVertex3i(-1, 12, -1);
 	glVertex3i(-1, 12, 4);
@@ -244,8 +317,9 @@ void cabineEsq1(void){
 }
 
 void cabineAtras(void){
+	glMaterialfv(GL_FRONT_AND_BACK , GL_AMBIENT_AND_DIFFUSE, corChassi);
 	glBegin(GL_POLYGON);
-	glColor3f(0.7f, 0.4f, 0.0f);
+	glNormal3f(0, 0, 1);
 	glVertex3i(2.5, 13, 4);
 	glVertex3i(2.5, 3, 4);
 	glVertex3i(-4, 3, 4);
@@ -254,9 +328,10 @@ void cabineAtras(void){
 }
 
 void cabineDir(void){
+	glMaterialfv(GL_FRONT_AND_BACK , GL_AMBIENT_AND_DIFFUSE, corChassi);
 	//Desenha Cabine lado esquerdo Parte 1
 	glBegin(GL_POLYGON);
-	glColor3f(0.8f, 0.5f, 0.0f);
+	glNormal3f(1, 0, 0);
 	glVertex3i(-1, 8, -5);
 	glVertex3i(-1, 3, -5);
 	glVertex3i(-1, 3, 4);
@@ -265,6 +340,7 @@ void cabineDir(void){
 	
 	//Desenha Cabine lado esquerdo Parte 2
 	glBegin(GL_POLYGON);
+	glNormal3f(1, 0, 0);
 	glVertex3i(-1, 12, 3);
 	glVertex3i(-1, 8, 3);
 	glVertex3i(-1, 8, 4);
@@ -273,6 +349,7 @@ void cabineDir(void){
 		
 	//Desenha Cabine lado esquerdo Parte 3
 	glBegin(GL_POLYGON);
+	glNormal3f(1, 0, 0);
 	glVertex3i(-1, 13, -1);
 	glVertex3i(-1, 12, -1);
 	glVertex3i(-1, 12, 4);
@@ -288,9 +365,10 @@ void cabineDir(void){
 }
 
 void cabineTeto(void){
+	glMaterialfv(GL_FRONT_AND_BACK , GL_AMBIENT_AND_DIFFUSE, corCalcada);
 	//Desenha Cabine Teto
 	glBegin(GL_POLYGON);
-	glColor3f(0.2f, 0.2f, 0.2f);
+	glNormal3f(0, 1, 0);
 	glVertex3i(-4, 13, 5);
 	glVertex3i(2, 13, 5);
 	glVertex3i(2, 13, 0);
@@ -299,9 +377,10 @@ void cabineTeto(void){
 }
 
 void cabineFrente(void){
+	glMaterialfv(GL_FRONT_AND_BACK , GL_AMBIENT_AND_DIFFUSE, corChassi);
 	//Desenha Cabine Frente
 	glBegin(GL_POLYGON);
-	glColor3f(0.7f, 0.4f, 0.0f);
+	glNormal3f(0, 0, -1);
 	glVertex3i(2, 6, -5);
 	glVertex3i(2, 1, -5);
 	glVertex3i(-4, 1, -5);
@@ -310,9 +389,10 @@ void cabineFrente(void){
 }
 
 void cabineBaixo(void){
+	glMaterialfv(GL_FRONT_AND_BACK , GL_AMBIENT_AND_DIFFUSE, corChassi);
 	//Desenha Cabine Baixo
 	glBegin(GL_POLYGON);
-	glColor3f(0.2f, 0.2f, 0.2f);
+	glNormal3f(0, 1, 0);
 	glVertex3i(-4, 13, 5);
 	glVertex3i(2, 13, 5);
 	glVertex3i(2, 13, -5);
@@ -323,53 +403,53 @@ void cabineBaixo(void){
 void farol(void){
 	//Desenha Farol
 	if(farolLuz)
-		glColor3i(1, 1, 0);
+		glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, corFarolOn);
 	else
-		glColor3f(0.5, 0.5, 0.5);
+		glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, corFarolOff);
 	//Desenha Farol
 	glutSolidSphere(1, 32, 32);
 }
 
 void cacamba(void){	
+glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, corCacamba);
 	glBegin(GL_QUADS);
-	
 	//Desenha cacamba esquerda
-    glColor3f(187/255.0, 255/255.0, 153/255.0);
+	glNormal3f(-1, 0, 0);
 	glVertex3i(-4, 13, 4);
 	glVertex3i(-4, 3, 4);
 	glVertex3i(-4, 3, 25);
 	glVertex3i(-4, 13, 30);
 	
 	//Desenha cacamba atrás
-    glColor3f(177/255.0, 245/255.0, 143/255.0);
+	glNormal3f(0, 0, 1);
 	glVertex3i(-4, 13, 30);
 	glVertex3i(-4, 3, 25);
 	glVertex3i(4, 3, 25);
 	glVertex3i(4, 13, 30);
 	
 	//Desenha cacamba direita
-    glColor3f(167/255.0, 235/255.0, 133/255.0);
+	glNormal3f(1, 0, 0);
 	glVertex3i(4, 13, 30);
 	glVertex3i(4, 3, 25);
 	glVertex3i(4, 3, 4);
 	glVertex3i(4, 13, 4);
 	
 	//Desenha cacamba frente
-    glColor3f(170/255.0, 240/255.0, 135/255.0);
+	glNormal3f(0, 0, -1);
 	glVertex3i(4, 13, 4);
 	glVertex3i(4, 3, 4);
 	glVertex3i(-4, 3, 4);
 	glVertex3i(-4, 13, 4);
 	
 	//Desenha cacamba teto
-    glColor3f(157/255.0, 225/255.0, 123/255.0);
+	glNormal3f(0, 1, 0);
 	glVertex3i(4, 13, 4);
 	glVertex3i(4, 13, 2);
 	glVertex3i(-4, 13, 2);
 	glVertex3i(-4, 13, 4);
 	
 	//Desenha cacamba baixo
-   	glColor3f(147/255.0, 215/255.0, 113/255.0);
+	glNormal3f(0, 1, 0);
 	glVertex3i(-4, 3, 4);
 	glVertex3i(-4, 3, 25);
 	glVertex3i(4, 3, 25);
@@ -378,10 +458,10 @@ void cacamba(void){
 }
 
 void pinheiro(){
+	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, corCopa);
 	glRotatef(270, 1, 0, 0);
-    glColor3f(167/255.0, 95/255.0, 133/255.0);
     glutSolidCone(5, 10, 10, 10);
-    glColor3f(128, 0, 0);
+    glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, corCaule);
     drawCylinder(6, 16, 10, 0.5);
 }
 
@@ -492,28 +572,26 @@ void caminhao(void){
 }
 
 void poste(void){
+	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, corPoste);
 	glPushMatrix();
-	glColor3f(0.8, 0.8, 0.8);
 	glRotatef(-90, 1, 0, 0);
 	drawCylinder(16, 16, 50, 1);
 	glPopMatrix();
 	
 	glPushMatrix();
-	glColor3f(0.8, 0.8, 0.8);
 	glTranslatef(10, 24.4,0);
 	glRotatef(-90, 0, 1, 0);
 	drawCylinder(16, 16, 20, 1);
 	glPopMatrix();
 	
 	glPushMatrix();
-	glColor3f(0.8, 0.8, 0.8);
 	glTranslatef(20, 23.5, 0);
 	glRotatef(90, 1, 0, 0);
 	drawCylinder(16, 16, 3, 1);
 	glPopMatrix();
 	
+	glMaterialfv(GL_FRONT_AND_BACK , GL_AMBIENT_AND_DIFFUSE, corLuminaria);
 	glPushMatrix();
-	glColor3f(1, 1, 0);
 	glTranslatef(20, 20.5, 0);
 	glRotatef(-90, 1, 0, 0);
 	drawCone(4, 2, 16, 16);
@@ -559,8 +637,8 @@ void desenha(void){
    glLightf( GL_LIGHT1, GL_SPOT_CUTOFF, 30.0);
    glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, direcaoSpot0);
    glLightf(GL_LIGHT1, GL_CONSTANT_ATTENUATION, 1); 
-   glLightf(GL_LIGHT1, GL_LINEAR_ATTENUATION, 0.001); 
-   glLightf(GL_LIGHT1, GL_QUADRATIC_ATTENUATION, 0.0001);  
+   glLightf(GL_LIGHT1, GL_LINEAR_ATTENUATION, 0.01); 
+   glLightf(GL_LIGHT1, GL_QUADRATIC_ATTENUATION, 0.001);  
    glPushMatrix();
    glTranslatef(0, 0, mov);
    glLightfv(GL_LIGHT1, GL_POSITION, posicaoLuz1);
@@ -573,33 +651,97 @@ void desenha(void){
    glLightf( GL_LIGHT2, GL_SPOT_CUTOFF, 30.0);
    glLightfv(GL_LIGHT2, GL_SPOT_DIRECTION, direcaoSpot0);
    glLightf(GL_LIGHT2, GL_CONSTANT_ATTENUATION, 1); 
-   glLightf(GL_LIGHT2, GL_LINEAR_ATTENUATION, 0.001); 
-   glLightf(GL_LIGHT2, GL_QUADRATIC_ATTENUATION, 0.0001);  
+   glLightf(GL_LIGHT2, GL_LINEAR_ATTENUATION, 0.01); 
+   glLightf(GL_LIGHT2, GL_QUADRATIC_ATTENUATION, 0.001);  
    glPushMatrix();
    glTranslatef(0, 0, mov);
    glLightfv(GL_LIGHT2, GL_POSITION, posicaoLuz2);
    glPopMatrix();
    
+   //Luz poste 1
+   glLightfv(GL_LIGHT3, GL_POSITION, posicaoLuz3);
+   glLightfv(GL_LIGHT3, GL_AMBIENT, desligado);
+   glLightfv(GL_LIGHT3, GL_DIFFUSE, branco);
+   glLightfv(GL_LIGHT3, GL_SPECULAR, desligado);
+   glLightf(GL_LIGHT3, GL_CONSTANT_ATTENUATION, 1);
+   glLightf(GL_LIGHT3, GL_LINEAR_ATTENUATION, 0.01);
+   glLightf(GL_LIGHT3, GL_QUADRATIC_ATTENUATION, 0.001);
+   
+   
+   
+   //Luz poste 2
+   glLightfv(GL_LIGHT4, GL_POSITION, posicaoLuz4);
+   glLightfv(GL_LIGHT4, GL_AMBIENT, desligado);
+   glLightfv(GL_LIGHT4, GL_DIFFUSE, branco);
+   glLightfv(GL_LIGHT4, GL_SPECULAR, desligado);
+   glLightf(GL_LIGHT4, GL_CONSTANT_ATTENUATION, 1);
+   glLightf(GL_LIGHT4, GL_LINEAR_ATTENUATION, 0.01);
+   glLightf(GL_LIGHT4, GL_QUADRATIC_ATTENUATION, 0.001);
+   
+  
+  
+   //Luz poste 3
+   glLightfv(GL_LIGHT5, GL_POSITION, posicaoLuz5);
+   glLightfv(GL_LIGHT5, GL_AMBIENT, desligado);
+   glLightfv(GL_LIGHT5, GL_DIFFUSE, branco);
+   glLightfv(GL_LIGHT5, GL_SPECULAR, desligado);
+   glLightf(GL_LIGHT5, GL_CONSTANT_ATTENUATION, 1);
+   glLightf(GL_LIGHT5, GL_LINEAR_ATTENUATION, 0.01);
+   glLightf(GL_LIGHT5, GL_QUADRATIC_ATTENUATION, 0.001);
+   
+    //Luz poste 4
+   glLightfv(GL_LIGHT6, GL_POSITION, posicaoLuz6);
+   glLightfv(GL_LIGHT6, GL_AMBIENT, desligado);
+   glLightfv(GL_LIGHT6, GL_DIFFUSE, branco);
+   glLightfv(GL_LIGHT6, GL_SPECULAR, desligado);
+   glLightf(GL_LIGHT6, GL_CONSTANT_ATTENUATION, 1);
+   glLightf(GL_LIGHT6, GL_LINEAR_ATTENUATION, 0.01);
+   glLightf(GL_LIGHT6, GL_QUADRATIC_ATTENUATION, 0.001);
+   
+    //Luz poste 5
+   glLightfv(GL_LIGHT7, GL_POSITION, posicaoLuz7);
+   glLightfv(GL_LIGHT7, GL_AMBIENT, desligado);
+   glLightfv(GL_LIGHT7, GL_DIFFUSE, branco);
+   glLightfv(GL_LIGHT7, GL_SPECULAR, desligado);
+   glLightf(GL_LIGHT7, GL_CONSTANT_ATTENUATION, 1);
+   glLightf(GL_LIGHT7, GL_LINEAR_ATTENUATION, 0.01);
+   glLightf(GL_LIGHT7, GL_QUADRATIC_ATTENUATION, 0.001);
+
+   
+  
+   // Liga/desliga luz poste
+   if(posteLuz){
+   	glEnable(GL_LIGHT3);
+   	glEnable(GL_LIGHT4);
+   	glEnable(GL_LIGHT5);
+   	glEnable(GL_LIGHT6);
+   	glEnable(GL_LIGHT7);
+   	posteChar = 'L';
+   }
+   else{
+   	glDisable(GL_LIGHT3);
+   	glDisable(GL_LIGHT4);
+   	glDisable(GL_LIGHT5);
+   	glDisable(GL_LIGHT6);
+   	glDisable(GL_LIGHT7);
+   	posteChar = 'D';
+   }
+     
    // Liga/desliga luz farol
    if (farolLuz){
    	  glEnable(GL_LIGHT1);
       glEnable(GL_LIGHT2);
-   }
-    
+      farolChar = 'L';
+   }   
    else{
    	  glDisable(GL_LIGHT1);
       glDisable(GL_LIGHT2);
+      farolChar = 'D';
    }
 
 	ceu();	
-	
-	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, corEstrada);
 	estrada();
-	
-	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, corCalcada);
 	calcada();
-	
-	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, corGrama);
 	grama();
 
 	
@@ -639,12 +781,14 @@ void desenha(void){
 	caminhao();	
 	glPopMatrix();
 	
+	glDisable(GL_LIGHTING);
+	
 	// Muda para projecao em 2D
    iniciaProjecaoOrtografica2D();
 
    // Renderizacao da mensagem em 2D
    char buffer[500];
-   sprintf(buffer, str, eyeX, eyeY, eyeZ, angulo, tamFonte);
+   sprintf(buffer, str, eyeX, eyeY, eyeZ, angulo, velocidade, farolChar, posteChar);
     
    int tam    = glutBitmapLength(GLUT_BITMAP_HELVETICA_18, (const unsigned char *) buffer);
    int textoX = (largura_vp - tam)/2;
@@ -700,10 +844,10 @@ void teclado(unsigned char ch, int x, int y)
        incrY = -1;
      }     
      else if (ch == 't') {
-     	b -= 1;
+     	b -= velocidade;
 	 }
 	 else if (ch == 'f') {
-	 	b += 1;
+	 	b += velocidade;
 	 }
 	 else if (ch == 'a') {
 	 	d -= 0.5;
@@ -714,6 +858,20 @@ void teclado(unsigned char ch, int x, int y)
 	else if(ch == '1') {
 	 	farolLuz = ! farolLuz;
 	 	glutPostRedisplay();
+	 }
+	 else if(ch == '2'){
+	 	posteLuz = ! posteLuz;
+	 	glutPostRedisplay();
+	 }
+	 else if(ch == 'l'){
+	 	conSol -= 0.1;
+	 	glutPostRedisplay();
+	 }
+	 else if(ch == '-' && velocidade <=9 && velocidade >1 ){
+	 	velocidade-=1;
+	 }
+	 else if(ch == '+' && velocidade >=1 && velocidade <9){
+	 	velocidade+=1;
 	 }
 }
 
@@ -780,12 +938,6 @@ void recalculaCena(int valor)
     if (d != 0)
     	if ((angCacamba + d) >= 0 && (angCacamba + d) <= 25)
     		angCacamba += d;
-
-     
-     anguloCubo += incrAnguloCubo;
-     
-     if (anguloCubo > 360)
-        anguloCubo -= 360;
      
      glutPostRedisplay();
         
